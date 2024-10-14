0074b35d16fa2aaeb22767e4de732e0ddd933a53
Nicolas Byl <nico@nicolas-byl.eu>
2024-10-14 11:43:43 +0000

    add test cases for backends

--- tests/backends/test_memory.py
+++ tests/backends/test_memory.py
@@ -2,14 +2,14 @@ from note_api.model import CreateNoteRequest, Note
 from note_api.backends import MemoryBackend
 from note_api.main import create_note, get_note, get_notes
 
-
 def test_save_and_get_item():
     backend = MemoryBackend()
-    id = create_note(CreateNoteRequest(
+    id = 'test12345'
+    backend.set(id, CreateNoteRequest(
         title='Test Note',
         description='Demo Note',
-    ), backend)
-    assert get_note(id, backend) == Note(title='Test Note', description='Demo Note', id=id)
+    ))
+    assert backend.get(id) == Note(title='Test Note', description='Demo Note', id=id)
 
 
 def test_save_and_get_items():
@@ -22,5 +22,5 @@ def test_save_and_get_items():
         title='Test Note 2',
         description='Demo Note 2',
     ), backend)
-    notes = get_notes(backend)
-    assert len(notes) == 2
+    keys = backend.keys()
+    assert len(keys) == 2
--- tests/backends/test_redis.py
+++ tests/backends/test_redis.py
@@ -1,19 +1,23 @@
+from fakeredis import FakeStrictRedis
+
 from note_api.model import CreateNoteRequest, Note
-from note_api.backends import MemoryBackend
+from note_api.backends import RedisBackend
 from note_api.main import create_note, get_note, get_notes
 
-
 def test_save_and_get_item():
-    backend = MemoryBackend()
-    id = create_note(CreateNoteRequest(
+    redis = FakeStrictRedis()
+    backend = RedisBackend(redis)
+    id = 'test12345'
+    backend.set(id, CreateNoteRequest(
         title='Test Note',
         description='Demo Note',
-    ), backend)
-    assert get_note(id, backend) == Note(title='Test Note', description='Demo Note', id=id)
+    ))
+    assert backend.get(id) == Note(title='Test Note', description='Demo Note', id=id)
 
 
 def test_save_and_get_items():
-    backend = MemoryBackend()
+    redis = FakeStrictRedis()
+    backend = RedisBackend(redis)
     create_note(CreateNoteRequest(
         title='Test Note',
         description='Demo Note',
@@ -22,5 +26,5 @@ def test_save_and_get_items():
         title='Test Note 2',
         description='Demo Note 2',
     ), backend)
-    notes = get_notes(backend)
-    assert len(notes) == 2
+    keys = backend.keys()
+    assert len(keys) == 2
