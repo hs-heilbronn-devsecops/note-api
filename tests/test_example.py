6dab2904b7f1c029e0ddaf191c067f4130bc0ed9
Aditya Gawade <39738448+Aditya3510@users.noreply.github.com>
2024-10-21 11:40:52 +0200

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,19 @@
-import pytest
+# import pytest
 
-from note_api.example import hello
+# from note_api.example import hello
+
+# @pytest.mark.parametrize(
+#     ("name", "expected"),
+#     [
+#         ("A. Musing", "Hello A. Musing!"),
+#         ("traveler", "Hello traveler!"),
+#         ("projen developer", "Hello projen developer!"),
+#     ],
+# )
+# def test_hello(name, expected):
+#     """Example test with parametrization."""
+#     assert hello(name) == expected
+
 
 @pytest.mark.parametrize(
     ("name", "expected"),
