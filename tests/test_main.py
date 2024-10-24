c22e9470c854beaf4343563234b1c804c53b98c9
Hendrik Schlehlein <haveachin@proton.me>
2024-10-24 18:15:03 +0000

    test: add update and get item test (#1)
    * ci: bump
    
    * test: add update and get item test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 from note_api.model import CreateNoteRequest, Note
 from note_api.backends import MemoryBackend
-from note_api.main import create_note, get_note, get_notes
+from note_api.main import create_note, get_note, get_notes, update_note
 
 
 def test_save_and_get_item():
@@ -10,7 +10,6 @@ def test_save_and_get_item():
         description='Demo Note',
     ), backend)
     assert get_note(id, backend) == Note(title='Test Note', description='Demo Note', id=id)
-
 
 def test_save_and_get_items():
     backend = MemoryBackend()
@@ -24,3 +23,15 @@ def test_save_and_get_items():
     ), backend)
     notes = get_notes(backend)
     assert len(notes) == 2
+
+def test_update_and_get_item():
+    backend = MemoryBackend()
+    id = create_note(CreateNoteRequest(
+        title='Test Note',
+        description='Demo Note',
+    ), backend)
+    update_note(id, CreateNoteRequest(
+        title='Updated Test Note',
+        description='Updated Demo Note',
+    ), backend)
+    assert get_note(id, backend) == Note(title='Updated Test Note', description='Updated Demo Note', id=id)
